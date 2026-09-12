f4f16c29511d4fc7f6cc3cb689f982b2ac2ddd62
joelmakescode <joel.ros@web.de>
2026-09-12 13:21:04 +0200

    Refactored PlayerDefaultData.java

--- src/main/java/org/thesplitting/src/data/player/PlayerData.java
+++ src/main/java/org/thesplitting/src/data/player/PlayerData.java
@@ -1,27 +1,49 @@
 package org.thesplitting.src.data.player;
 
 import org.thesplitting.src.data.item.CollectableItems.CollectableItems;
+import org.thesplitting.src.data.item.PlayerItems.InventoryManagerItem;
+import org.thesplitting.src.data.item.PlayerItems.StartBookItem;
+import org.thesplitting.src.data.item.PlayerItems.StartSwordItem;
 import org.thesplitting.src.data.player.PlayerRoles.PlayerRole;
 import org.thesplitting.src.data.player.PlayerRoles.PlayerRoles;
 
+import java.util.HashMap;
+
+import static org.thesplitting.src.services.services.itemservice.ItemService.getItems;
+
 public record PlayerData(String name, PlayerRole playerRole, PlayerSettings playerSettings, PlayerStats playerStats, PlayerInventory playerInventory, CollectableItems collectableItems) {
 
-    // TODO: LOOK ON THIS SCHEMA LATER, PLAYERDEFAULTDATA MIGHT BE OBSOLETE
     public static PlayerData create(String playerName) {
         return new PlayerData(
                 playerName,
                 new PlayerRole(PlayerRoles.NOVICE.toString()),
-                new PlayerSettings(PlayerDefaultData.SETTINGS.getIsStarter()),
+                new PlayerSettings(1),
                 new PlayerStats(
-                        PlayerDefaultData.STATS.getMaxHealth(),
-                        PlayerDefaultData.STATS.getLevel(),
-                        PlayerDefaultData.STATS.getInferno(),
-                        PlayerDefaultData.STATS.getBolt(),
-                        PlayerDefaultData.STATS.getWater_flow(),
-                        PlayerDefaultData.STATS.getGreen_flame()
+                        4,
+                        1,
+                        1,
+                        1,
+                        1,
+                        1
                 ),
-                new PlayerInventory(PlayerDefaultData.INVENTORY.inventorySlots()),
-                new CollectableItems(PlayerDefaultData.COLLECTABLES.getCollectableItems())
+                new PlayerInventory(new String[]{
+                        StartSwordItem.ID,
+                        null,
+                        null,
+                        null,
+                        InventoryManagerItem.ID,
+                        null,
+                        null,
+                        null,
+                        StartBookItem.ID
+                }),
+                createDefaultCollectableItems()
         );
+    }
+
+    private static CollectableItems createDefaultCollectableItems() {
+        CollectableItems collectableItems = new CollectableItems(new HashMap<>());
+        getItems().forEach(collectableItems::addCollectableItem);
+        return collectableItems;
     }
 }
